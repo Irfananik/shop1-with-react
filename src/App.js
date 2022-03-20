9e35b995aff93f758e6f15dd132b67dc6af3b149
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-20 15:14:51 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Header from './Components/Header/Header';
 import Allproducts from './Components/Allproducts/Allproducts';
 import { useState } from 'react';
 
+
 function App() {
   const [count,setCount] = useState(0)
   const setCartCount = () =>{
     setCount(count + 1)
   }
+
+ 
   return (
     <div className="App">
       <Header count = {count} ></Header>
