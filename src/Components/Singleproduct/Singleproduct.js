1fb010583b0d37bb9f30162e6c9eacbb0c80f105
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-20 13:31:37 +0600

    modified UI

--- src/Components/Singleproduct/Singleproduct.js
+++ src/Components/Singleproduct/Singleproduct.js
@@ -4,10 +4,20 @@ import './Singleproducts.css'
 const Singleproduct = (props) => {
    console.log(props.product.title)
     return (
-        <div className="col-md-3 p-3 mt-5 card border">
-           <h4>Product Title: {props.product.title.slice(1,10)}</h4>
-           <h6>Product price: {props.product.price}</h6>
-           <p>Product discription: {props.product.description.slice(1,30)}</p>
+        <div className="col-md-3 p-4 mt-5 card">
+            <div className="card-body">
+                <img className="img-fluid single-img" src={props.product.image} alt="" />
+                <div className="mt-3">
+                    <h4>Product Title: {props.product.title.slice(1,10)}</h4>
+                    <h6>Product price: {props.product.price}</h6>
+                    <p>Product discription: {props.product.description.slice(1,30)}</p>
+                    <div className="d-flex justify-content-around">
+                        <button type="button" class="btn btn-primary me-2">AddTOcard</button>
+                        <button type="button" class="btn btn-danger">Delete</button>
+                        <button type="button" class="btn btn-success ms-2">Details</button>
+                     </div>
+                </div>
+            </div>
         </div>
     );
 };
